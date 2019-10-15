168e7b10f11bf412d372fed0605d606b8f1689ab
Hamed Masafi <hamed.masafi@gmail.com>
2019-10-15 11:55:35 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <Inputs :groups="groups" :model="model" />
+
+<div class="d-print-none">
+    <b-button variant="primary">چاپ</b-button>
+    <b-button variant="outline-primary"><font-awesome-icon icon="save" />ذخیره</b-button>
+    <b-button variant="outline-primary"><font-awesome-icon icon="folder-open" />گشودن</b-button>
+</div>
+
+    <Form1 :model="model" />
+
+  {{model.name}}
+  </div>
+</template>
+
+<script>
+import Inputs from './components/Inputs.vue'
+import Form1 from './components/Form1.vue'
+
+import { groups, props, forms } from './data'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/style.css'
+
+export default {
+  name: 'app',
+  data(){
+      return{
+        pages: ['a','b','c','d'],
+        groups: groups,
+        props: props,
+        forms: forms,
+        model: {
+          a:3,
+          name_full(){return this["name"] + " " + this["last_name"];}
+        }
+      }
+  },
+  components: {
+    Inputs, Form1
+  }
+}
+
+Vue.use(BootstrapVue)
+
+</script>
+
+<style>
+@import 'http://cdn.font-store.ir/nazanin.css';
+@import 'https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css';
+</style>
+<style>
+#app {
+  direction: rtl;
+  font-family: 'nazanin';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Inputs.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="editor" class="d-print-none">
+  <b-tabs content-class="mt-3">
+    <b-tab v-for="group in groups" v-bind:key="group" :title="group.title">
+      <p>{{ group.title }}</p>
+      <b-form-group v-for="field in group.fields" :key="field"
+          v-bind:label="field.title">
+
+          <b-form-select v-model="model[field.name + '_sex']" :options="['آقا', 'خانم']" v-if="field.type === 'person'" ></b-form-select>
+          <b-form-input :name="field.name" :placeholder="field.title + ' فارسی'" v-model="model[field.name]" 
+            :type="field.type === 'number' ? 'number' : 'test'"></b-form-input>
+          <b-form-input v-model="model[field.name + '_en']" :placeholder="field.title + ' انگلیسی'" v-if="field.tr"></b-form-input>
+      </b-form-group>
+    </b-tab>
+  </b-tabs>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: "Inputs",
+    props: {
+        groups: Array,
+        model: Object
+    }
+}
+</script>
+
+<style scoped>
+  #editor {
+    margin: 20px;
+    padding: 20px;
+  }
+</style>
