a8dfc4208c2aa6b7bed43b3e106a490e2efa4a59
Hamed Masafi <hamed.masafi@gmail.com>
2019-11-13 20:33:07 +0330

    ui minor improvment

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app id="app" dark>
+  <v-app id="app" dark style="background-color: white">
     <v-navigation-drawer class="d-print-none"
         v-model="drawer"
         app right>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -9,4 +9,40 @@ export default new Vuetify({
   icons: {
     iconfont: 'mdi',
   },
+  theme: {
+    themes: {
+      light: {
+        primary: '#3f51b5',
+        secondary: '#e91e63',
+        accent: '#9c27b0',
+        error: '#f44336',
+        warning: '#ff9800',
+        info: '#03a9f4',
+        success: '#4caf50'
+      }
+    }
+  }
 });
+
+/*
+import colors from 'vuetify/es5/util/colors'
+
+      {
+      primary: colors.indigo.base,
+      secondary: colors.pink.base,
+      accent: colors.purple.base,
+      error: colors.red.base,
+      warning: colors.orange.base,
+      info: colors.light-blue.base,
+      success: colors.green.base
+      }*/
+
+      /*      {
+      primary: #3f51b5,
+      secondary: #e91e63,
+      accent: #9c27b0,
+      error: #f44336,
+      warning: #ff9800,
+      info: #03a9f4,
+      success: #4caf50
+      }*/
